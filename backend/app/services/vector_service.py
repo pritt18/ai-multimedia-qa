a84f8275931920333c64f29189985f71f438f35e
pritt18 <pritamgangurde18@gmail.com>
2026-05-28 21:48:59 +0530

    Optimized embeddings memory usage

--- backend/app/services/vector_service.py
+++ backend/app/services/vector_service.py
@@ -1,27 +1,55 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import (
+    RecursiveCharacterTextSplitter
+)
 
 from langchain_community.vectorstores import FAISS
 
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
-
-embedding = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
+from langchain_community.embeddings import (
+    HuggingFaceEmbeddings
 )
 
 
-def create_vector_store(text, file_id):
+def get_embedding():
 
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=100
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
-    chunks = splitter.split_text(text)
 
-    vectorstore = FAISS.from_texts(
+def create_vector_store(
+    text,
+    file_id
+):
+
+    text_splitter = (
+        RecursiveCharacterTextSplitter(
+            chunk_size=500,
+            chunk_overlap=100
+        )
+    )
+
+    chunks = text_splitter.split_text(text)
+
+    embedding = get_embedding()
+
+    vector_store = FAISS.from_texts(
         chunks,
         embedding
     )
 
-    vectorstore.save_local(f"vectors/{file_id}")
+    vector_store.save_local(
+        f"vectors/{file_id}"
+    )
+
+
+def load_vector_store(
+    file_id
+):
+
+    embedding = get_embedding()
+
+    return FAISS.load_local(
+        f"vectors/{file_id}",
+        embedding,
+        allow_dangerous_deserialization=True
+    )
